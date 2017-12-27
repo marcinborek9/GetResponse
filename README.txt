e2d13bf32b098362b65498ad787bc8beb5a55939
Marcin <marcinborek9@gmail.com>
2017-12-27 08:29:39 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+How to install:
+
+1. download and install node.js from https://nodejs.org/en/
+2. open windows powershell / git bash / bash
+3. go to directory with all files (cd GetResponse)
+3. npm install 
+4. npm install --global gulp-cli
+5. npm install --save-dev gulp-watch
+6. gulp watch
+7. open in browser localhost:3000
---
+++ public/scripts/main.js
@@ -0,0 +1,302 @@
+var obj = [
+  {
+    'firstRow': '1. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '2. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '3. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '4. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '5. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '6. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '8. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '9. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '10. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '11. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '12. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '13. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '15. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '16. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '17. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '18. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '19. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '20. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '22. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '23. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '24. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+  {
+    'firstRow': '25. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Turkey Melt',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '26. Bode Shake',
+    'secondRow': 'Ham and Swiss Roll Ups',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn with Mozzarella and Tomato Slices',
+    'fifthRow': 'Turkey Melt',
+    'sixthRow': 'low-carb'
+  },
+  {
+    'firstRow': '27. Bode Shake',
+    'secondRow': 'Grilled Steak (HC)',
+    'thirdRow': 'Bode Shake',
+    'fourthRow': 'Bode Burn and Whole-Wheat English Muffin with Butter Spray',
+    'fifthRow': 'Garlic Lime Chicken (HC)',
+    'sixthRow': 'high-carb'
+  },
+]
+
+
+var index = 0;
+var day = 0;
+var week = 0;
+var k = 0;
+displayContent(false);
+
+document.querySelector('.next').addEventListener('click', function() {
+  document.querySelector('.previous').style.visibility = 'visible';
+  k += 6;
+  displayContent(false);
+  if (obj.length <= k + 6) {
+    document.querySelector('.next').style.visibility = 'hidden';
+  }
+});
+
+document.querySelector('.previous').addEventListener('click', function() {
+  k -= 6;
+  displayContent(true)
+  document.querySelector('.next').style.visibility = 'visible';
+  if (k <= 0) {
+    document.querySelector('.previous').style.visibility = 'hidden';
+  }
+});
+
+function changeProgressBar() {
+  var elements = document.getElementsByClassName('progress-bar__element'), i, len;
+  for (i = 0, len = elements.length; i < week; i++) {
+    elements[i].style.background = '#b2c200';
+    if ((i + 1) === week) {
+      elements[i].style.background = '#ffffff';
+      elements[i].style.border = '1px solid #dbdbdb';
+    }
+  }
+}
+
+function changeBackProgressBar() {
+  var elements = document.getElementsByClassName('progress-bar__element'), i, len;
+  for (i = week, len = elements.length; i <= week; i++) {
+    elements[i].style.background = '#dbdbdb';
+    elements[i - 1].style.background = '#ffffff';
+    elements[i - 1].style.border = '1px solid #dbdbdb';
+  }
+}
+
+function changeWeek(rev) {
+  if (rev === false) {
+    document.querySelector('.weekNumber').textContent = ++week;
+    changeProgressBar()
+  } else {
+    document.querySelector('.weekNumber').textContent = --week;
+    changeBackProgressBar()
+  }
+}
+
+function changeDay(rev) {
+  if (rev === false) {
+    for (var i = 1; i <= 7; i++) {
+      document.querySelector('.dayNumber-' + i).textContent = day + i;
+    }
+    day += 7;
+  } else {
+    day -= 7;
+    var j = 0;
+      for (var i = 7; i >= 1; i--) {
+        document.querySelector('.dayNumber-' + i).textContent = day - j++;
+      }
+    }
+}
+
+function displayContent(rev) {
+  for (var j = 0; j <= 5; ++j ) {
+    var cell = document.querySelector('#column-' + j).querySelectorAll('.column-content__text');
+    cell[0].textContent = obj[j + k]['firstRow']
+    cell[1].textContent = obj[j + k]['secondRow']
+    cell[2].textContent = obj[j + k]['thirdRow']
+    cell[3].textContent = obj[j + k]['fourthRow']
+    cell[4].textContent = obj[j + k]['fifthRow']
+    cell[5].textContent = obj[j + k]['sixthRow']
+  }
+  changeWeek(rev);
+  changeDay(rev);
+}
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+  [].map.call(document.querySelectorAll('.hamburger'), function(el) {
+    el.classList.toggle('is-active');
+  });
+  });
+
+  document.querySelector('.hamburger').addEventListener('click', function() {
+    [].map.call(document.querySelectorAll('.header-navbar'), function(el) {
+      el.classList.toggle('menu-hide');
+    });
+});
+
+var dropDowns = Array.from(document.querySelectorAll('.header-navbar__list--element'));
+var handleClick = function handleClick(e) {
+  e.preventDefault();
+  dropDowns.forEach(function (node) {
+    node.classList.remove('active');
+  });
+  e.currentTarget.classList.add('active');
+};
+dropDowns.forEach(function (node) {
+  node.addEventListener('click', handleClick);
+});
